9a601e7062ae6e70e242fd65add841280a8e9f48
Megan McHugh <megan.mchugh@gmail.com>
2022-10-11 18:56:31 -0500

    started readme, added reset.css, added variable to js file, and started basics of html with links

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Weather-Repository
+
+## Description
+
+## Usage
+
+## Credits
+
+reset.css from:
+https://github.com/necolas/normalize.css/blob/master/normalize.css
+
+## License
+See the MIT license in the repository.
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,9 @@
 var APIkey = "05c258fa745fbc728c4ebf2eb6a9a431";
+var city;
+var state;
+var country;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
+
+fetch(queryURL);
+
+console.log(queryURL);
